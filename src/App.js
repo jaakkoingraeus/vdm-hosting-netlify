cc7102684332e0f64691f44a7f72244d5acfa540
Hiski Huovila <hiski.huovila@gmail.com>
2021-05-04 22:14:57 +0300

    navbar, searchbar and tiles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarContainer from "./components/NavbarContainer.jsx";
+import Tile from "./components/Tile.jsx";
+import {Form, FormControl, Button, CardDeck } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarContainer></NavbarContainer>
+      <div className="Frontpage page">
+        <div className="FrontpageTextContainer"></div>
+        <div className="Video"></div>
+      </div>
+      <div className="Search page">
+        <div className="SearchBarContainer">
+        <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+        </Form>
+        </div>
+        <div className="CarouselContainer">
+        <CardDeck>
+          <Tile imagesrc="a" text=""></Tile>
+          <Tile imagesrc="a" text="a"></Tile>
+          <Tile imagesrc="a" text="a"></Tile>
+        </CardDeck>
+        </div>
+      </div>
+      <div className="Blogs page"></div>
+      <div className="Comments page"></div>
+      <div className="About page"></div>
     </div>
   );
 }
---
+++ src/components/Tile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function Tile(props){
+
+    const text = props.text
+    let content = ""
+  
+    if (text === "") {
+    content =
+    <Card.ImgOverlay variant="top" src={props.imagesrc} />
+    } else {
+    content = <div>
+    <Card.Img variant="top" src={props.imagesrc} />
+    <Card.Body>
+        <Card.Text>
+           {props.text}
+        </Card.Text>
+    </Card.Body>
+    </div>
+    }
+    return(
+        <Card style={{ maxWidth: '18rem' }}>
+        
+        {content}
+      </Card>
+    )
+}
+export default Tile
