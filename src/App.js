ffc4becf55986e0eacfb07f52c365edf899edb42
Hiski Huovila <hiski.huovila@gmail.com>
2021-05-05 11:54:06 +0300

    container kikkailua

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarContainer from "./components/NavbarContainer.jsx";
 import Tile from "./components/Tile.jsx";
-import {Form, FormControl, Button, CardDeck } from 'react-bootstrap'
+import {Form, FormControl, Button, CardDeck, Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
@@ -13,19 +13,29 @@ function App() {
         <div className="Video"></div>
       </div>
       <div className="Search page">
-        <div className="SearchBarContainer">
-        <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-        </Form>
-        </div>
-        <div className="CarouselContainer">
-        <CardDeck>
-          <Tile imagesrc="a" text=""></Tile>
-          <Tile imagesrc="a" text="a"></Tile>
-          <Tile imagesrc="a" text="a"></Tile>
-        </CardDeck>
-        </div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col>
+            <div className="SearchBarContainer">
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            </div>
+            </Col>
+          </Row>
+          <Row className="mt-2">
+            <Col>
+            <div className="CarouselContainer">
+            <CardDeck>
+              <Tile imagesrc="a" text=""></Tile>
+              <Tile imagesrc="a" text="a"></Tile>
+              <Tile imagesrc="a" text="a"></Tile>
+            </CardDeck>
+            </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
       <div className="Blogs page"></div>
       <div className="Comments page"></div>
