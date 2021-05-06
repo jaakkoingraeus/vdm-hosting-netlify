3d4c80391342600d2d897d3bd0af2438321a550d
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-05-06 11:15:53 +0300

    Add ico and img style navbar

--- src/components/NavbarContainer.jsx
+++ src/components/NavbarContainer.jsx
@@ -5,30 +5,32 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import Logo from '../ico/sitraLogo.svg'
 function NavbarContainer(props) {
     return(
-<Navbar bg="light" expand="lg" fixed="top">
-    <Navbar.Brand href="#frontpage">React-Bootstrap</Navbar.Brand>
+<Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar.Brand href="#frontpage" className="p-lg-3 p-sm-2">
+        <img width="200" src={Logo}/>
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-            <Nav.Link href="#frontpage">Home</Nav.Link>
-            <Nav.Link href="#search">Search</Nav.Link>
-            <Nav.Link href="#blogs">Blogs</Nav.Link>
-            <Nav.Link href="#comments">Comments</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#search" className="navLinkCustom">Vihertutka</Nav.Link>
+            <Nav.Link href="#blogs" className="navLinkCustom">Blogit</Nav.Link>
+            <Nav.Link href="#comments" className="navLinkCustom">Keskustelu</Nav.Link>
+            <Nav.Link href="#about" className="navLinkCustom">Meistä</Nav.Link>
+            {/**<NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown>*/}
         </Nav>
-        <Form inline>
+        {/**<Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form>*/}
     </Navbar.Collapse>
 </Navbar>
     )
