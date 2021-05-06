6355b201be4bf408fd0f02080d00707d41288b7d
Jaakko Ingraeus <jaakko.ingraeus@aalto.fi>
2021-05-06 11:33:16 +0300

    Frontpage graphics + slogan

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor' //https://www.npmjs.com/package/react-scrollable-anchor
+import VaroViherpesuainetta from '../ico/varoViherpesuainetta.svg'
+import Image from 'react-bootstrap/Image'
 
 function Frontpage(props) {
     return(
         <ScrollableAnchor id={'frontpage'}>
     <div className="page d-flex flex-column justify-content-center">
-        <div className="FrontpageTextContainer d-flex justify-content-center">
-            <h1>What even is this</h1>
+        <div className="FrontpageTextContainer d-flex justify-content-center flex-column">
+            <Image src={VaroViherpesuainetta} fluid/>
+            <h1 className="callToActionText">Testaa vaatteittesi todellinen vihreys <a href="#search" className="sloganLink">Vihertutkalla</a></h1>
         </div>
         <video autoPlay muted loop id="frontpageVideo">
             <source src="frontpageVideoTemp.mp4" type="video/mp4"/>
