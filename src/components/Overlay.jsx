f361e6a2848c9873c025151b11f3c732c52f7be9
Hiski Huovila <hiski.huovila@gmail.com>
2021-05-17 14:49:48 +0300

    elsint warning disabled in overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import { useEffect, useState } from 'react'
 import {Media,  Button, Fade, Container, Row } from 'react-bootstrap'
