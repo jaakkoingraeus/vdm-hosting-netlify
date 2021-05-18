d727c46a9e9080c6368e6dc99434c26dbd3e994d
Hiski Huovila <hiski.huovila@gmail.com>
2021-05-18 16:11:40 +0300

    divider fixes

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -10,10 +10,11 @@ function Divider(props) {
         height: '20vw',
         color: props.textColor,
         backgroundColor: props.color,
+        padding: '0vw 10vw'
     }
     return(
-        <div style={bgImage} className=" d-none d-md-flex justify-content-center align-items-center p-5">
-            <h3 className="dividerText">{props.text}</h3>
+        <div style={bgImage} className=" d-none d-md-flex flex-column justify-content-center align-items-center">
+            <h1 className="dividerText">{props.text}</h1>
         </div>
     )
     
