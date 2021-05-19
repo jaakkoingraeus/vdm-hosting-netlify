a7db9a79928db68d9101497882ad1dbc4d155fe2
Hiski Huovila <hiski.huovila@gmail.com>
2021-05-19 19:12:11 +0300

    video page

---
+++ src/pages/Video.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import ScrollableAnchor from 'react-scrollable-anchor' //https://www.npmjs.com/package/react-scrollable-anchor
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+function Video(props) {
+    return(
+    <ScrollableAnchor id={'campaignInfo'}>
+    <div className="page"> 
+    <Container>
+        <Row>
+        <Header text="Kampanjasta" borderColor="#ffe749"></Header>
+        </Row>
+            <Row>
+            <Col>       
+                 <Card className="shadow">
+                    <Card.Body>
+                        <Card.Title>Varo viherpesua! -kampanja</Card.Title>
+                        <Card.Text className="campaignText">
+                    Vaateteollisuudessa koventuneet vaatimukset, kilpailu ja voiton tavoittelu ovat johtaneeet kuluttajia sokaisevaan viherpesuun. Kiillotetulla kuluttajaviestinnällä ja markkinoinnilla kätketään tuhoisat ympäristövaikutukset sekä ihmis- ja eläintenoikeusrikkomukset.
+                    </Card.Text>
+                    <Card.Title>Miksi viherpesua pitäisi varoa?</Card.Title>
+                        <Card.Text className="campaignText">
+                   Vaatealan viherpesu on laajamittaista ja salakavalaa. Sen tunnistaminen auttaa tekemään kestäviä valintoja, jotka estävät ilmastonmuutosta, vähentävät köyhyyttä ja pelastavat henkiä. 
+                    </Card.Text>
+                    <Card.Title>Me kerromme vaatteidesi todellisen vihreyden</Card.Title>
+                        <Card.Text className="campaignText">
+                   Autamme tekemään oikeita valintoja viherpesun keskellä. Uskomme, että voit näyttää hyvältä, säästää lompakkoa ja pelastaa maapalloa samaan aikaan! Lue lisää <a href="#blogs">blogista</a>.
+                    </Card.Text>
+                    </Card.Body>
+            </Card>
+            </Col>
+            <Col className="d-none d-sm-none d-md-none d-lg-none d-xl-block">
+            <div className="statBox">
+                <h1>10%</h1>
+                <p className="statDesc">Vuotuisista globaaleista hiilidioksidipäästöistä aiheutuu vaateteollisuuden tuotannosta*</p>
+            </div>
+            <div className="statBox">
+                <h1>98%</h1>
+                <p className="statDesc">Maailman vaatetuotannon työntekijöistä ansaitsee kuukaudessa vähemmän, mitä normaali elintaso vaatii.*</p>
+            </div>
+            <div className="statBox">
+                <h1>100 milj</h1>
+                <p className="statDesc">Eläintä tapetaan vuosittain vaateollisuutta varten turkistarhauksessa*</p>
+            </div>
+            </Col>
+            
+            </Row>
+    </Container>
+    </div>
+    </ScrollableAnchor>
+    )
+}
+export default Video
